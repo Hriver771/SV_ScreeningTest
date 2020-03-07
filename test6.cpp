fe9f5e0fef58bf0fa18664275bdd0fe387709f72
HaMinJi <h.river771@gmail.com>
2020-03-07 16:13:21 +0900

    subtract mincost of Row and Colum

--- test6.cpp
+++ test6.cpp
@@ -1,6 +1,7 @@
 # include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,16 +17,84 @@ void PrintMatrix(const vector<vector<int>> mat)
 	}
 }
 
-int main()
+int GetMaxCost_Matrix(const vector<vector<int>> Cost)
+{
+	vector<int> maxCost_inRow;
+	for (vector<int> Row : Cost)
+	{
+		maxCost_inRow.push_back(*max_element(Row.begin(), Row.end()));
+	}
+	return *max_element(maxCost_inRow.begin(), maxCost_inRow.end());
+}
+
+void HungarianAlgo(vector<vector<int>> Cost, const int N, const int M, const int Mode, int & TotalCost, vector<int> AssignmentIndex)
+{
+	//정방향으로 만들기
+	if (N < M)
+	{
+		int maxCost = GetMaxCost_Matrix(Cost);
+		for (int i = 0; i < M - N; i++)
+		{
+			vector<int> fictionalRow(M, maxCost);
+			Cost.push_back(fictionalRow);
+		}
+	}
+	else if (N > M)
+	{
+		int maxCost = GetMaxCost_Matrix(Cost);
+		for (int i = 0; i < N - M; i++)
+		{
+			for (vector<int> Row : Cost)
+			{
+				Row.push_back(maxCost);
+			}
+		}
+	}
+	PrintMatrix(Cost);
+
+	int matrixSize = Cost.size();
+
+	//row에서 가장 작은 값 빼주기
+	for (int i = 0; i < matrixSize; i++)
+	{
+		int minCost = *min_element(Cost[i].begin(), Cost[i].end());
+		for (int j = 0; j < matrixSize; j++)
+		{
+			Cost[i][j] -= minCost;
+		}
+	}
+	PrintMatrix(Cost);
+
+	//colum에서 가장 작은 값 빼주기
+	for (int j = 0; j < matrixSize; j++)
+	{
+		vector<int> colum;
+		for (vector<int> Row : Cost)
+		{
+			colum.push_back(Row[j]);
+		}
+		int minCost = *min_element(colum.begin(), colum.end());
+		for (int i = 0; i < matrixSize; i++)
+		{
+			Cost[i][j] -= minCost;
+		}
+	}
+	PrintMatrix(Cost);
+}
+
+void main()
 {
 	int N;
 	int M;
+	int Mode;
 	string strInput;
 	vector<vector<int>> OriginMatrix;
+	int TotalCost;
+	vector<int> AssignmentIndex;
 
-	cout << "Enter the number of N: ";
+	cout << "Enter the number of worker(N): ";
 	cin >> N;
-	cout << "Enter the number of M: ";
+	cout << "Enter the number of job(M): ";
 	cin >> M;
 	getchar(); //"\n" 무시하기 위해서
 
@@ -56,16 +125,18 @@ int main()
 	if (OriginMatrix.size() != N)
 	{
 		cout << "Input error : not mached colum size" << endl;
+		return;
 	}
 	for (vector<int> row : OriginMatrix)
 	{
 		if (row.size() != M)
 		{
 			cout << "Input error : not mached row size" << endl;
+			return;
 		}
 	}
 
-	PrintMatrix(OriginMatrix);
-
+	Mode = 0;
+	HungarianAlgo(OriginMatrix, N, M, Mode, TotalCost, AssignmentIndex);
 
 }
