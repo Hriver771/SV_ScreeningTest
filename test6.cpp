fec9224d7504663064fe1d8da87e9a95d261aba0
HaMinJi <h.river771@gmail.com>
2020-03-07 23:13:46 +0900

    ..

--- test6.cpp
+++ test6.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void PrintMatrix(const vector<vector<int>> mat)
 {
+	cout << "------------------------------------------------" << endl;
 	for (int i = 0; i < mat.size(); i++)
 	{
 		for (int j = 0; j < mat[i].size(); j++)
@@ -15,6 +16,7 @@ void PrintMatrix(const vector<vector<int>> mat)
 		}
 		cout << endl;
 	}
+	cout << "------------------------------------------------" << endl;
 }
 
 int GetMaxCost_Matrix(const vector<vector<int>> Cost)
@@ -52,34 +54,127 @@ void HungarianAlgo(vector<vector<int>> Cost, const int N, const int M, const int
 	}
 	PrintMatrix(Cost);
 
-	int matrixSize = Cost.size();
+	int MatrixSize = Cost.size();
 
 	//row에서 가장 작은 값 빼주기
-	for (int i = 0; i < matrixSize; i++)
+	for (int i = 0; i < MatrixSize; i++)
 	{
 		int minCost = *min_element(Cost[i].begin(), Cost[i].end());
-		for (int j = 0; j < matrixSize; j++)
+		for (int j = 0; j < MatrixSize; j++)
 		{
 			Cost[i][j] -= minCost;
 		}
 	}
 	PrintMatrix(Cost);
 
-	//colum에서 가장 작은 값 빼주기
-	for (int j = 0; j < matrixSize; j++)
+	////colum에서 가장 작은 값 빼주기
+	//for (int j = 0; j < matrixSize; j++)
+	//{
+	//	vector<int> colum;
+	//	for (vector<int> Row : Cost)
+	//	{
+	//		colum.push_back(Row[j]);
+	//	}
+	//	int minCost = *min_element(colum.begin(), colum.end());
+	//	for (int i = 0; i < matrixSize; i++)
+	//	{
+	//		Cost[i][j] -= minCost;
+	//	}
+	//}
+	//PrintMatrix(Cost);
+
+	vector<int> CoveredRow;
+	vector<int> CoveredCol;
+	vector<vector<bool>> StarZeros(MatrixSize, vector<bool>(MatrixSize, false));
+	vector<vector<bool>> PrimeZeros(Cost.size(), vector<bool>(Cost.size(), false));
+
+	//step 2
+	for (int i = 0; i < MatrixSize; i++)
 	{
-		vector<int> colum;
-		for (vector<int> Row : Cost)
+		for (int j = 0; j < MatrixSize; j++)
 		{
-			colum.push_back(Row[j]);
-		}
-		int minCost = *min_element(colum.begin(), colum.end());
-		for (int i = 0; i < matrixSize; i++)
-		{
-			Cost[i][j] -= minCost;
+			if (Cost[i][j] == 0)
+			{
+				StarZeros[i][j] = true;
+			}
 		}
 	}
-	PrintMatrix(Cost);
+	//step 3
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			if (StarZeros[i][j] == true)
+			{
+				CoveredCol.push_back(j);
+			}
+		}
+	}
+	if (CoveredCol.size() == MatrixSize)
+	{
+		//종료
+	}
+
+	//step 4
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		auto iterR = find(CoveredRow.begin(), CoveredRow.end(), i);
+		if (iterR != CoveredRow.end())
+		{
+			for (int j = 0; j < MatrixSize; j++)
+			{
+				auto iterC = find(CoveredCol.begin(), CoveredCol.end(), j);
+				if (iterC != CoveredCol.end())
+				{
+					if (Cost[i][j] == 0)
+					{
+						PrimeZeros[i][j] = true;
+						auto iterStarRow = find(StarZeros[i].begin(), StarZeros[i].end(), true);
+
+						if (iterStarRow == StarZeros[i].end())
+						{
+							//5단계로 간다
+						}
+						else
+						{
+							CoveredRow.push_back(i);
+							int removeCol = distance(StarZeros[i].begin(), iterStarRow);
+							auto removeColIter = find(CoveredCol.begin(), CoveredCol.end(), removeCol);
+							CoveredCol.erase(removeColIter);
+							
+						}
+					}
+				}
+
+			}
+		}
+		//6단계로 감
+	}
+
+	//step 5
+	vector<vector<bool>> NewZeros(MatrixSize, vector<bool>(MatrixSize, false));
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			if (PrimeZeros[i][j] == true)
+			{
+				for (int k = 0; k < MatrixSize; k++)
+				{
+					if (StarZeros[k][j] == true)
+					{
+						break;
+					}
+					if (k + 1 == MatrixSize)
+					{
+						NewZeros[i][j] = true;
+
+					}
+				}
+
+			}
+		}
+	}
 }
 
 void main()
@@ -138,5 +233,7 @@ void main()
 
 	Mode = 0;
 	HungarianAlgo(OriginMatrix, N, M, Mode, TotalCost, AssignmentIndex);
+
+
 
 }
