f1962df036890c0ed7d38729457150faf416bb4a
HaMinJi <h.river771@gmail.com>
2020-03-08 03:24:28 +0900

    will fix step5

--- test6.cpp
+++ test6.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+
+
+void step4(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros);
+void step5(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros);
+void step6(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros);
+
+
 void PrintMatrix(const vector<vector<int>> mat)
 {
 	cout << "------------------------------------------------" << endl;
@@ -19,6 +29,44 @@ void PrintMatrix(const vector<vector<int>> mat)
 	cout << "------------------------------------------------" << endl;
 }
 
+void PrintMatrixB(const vector<vector<bool>> mat)
+{
+	cout << "------------------------------------------------" << endl;
+	for (int i = 0; i < mat.size(); i++)
+	{
+		for (int j = 0; j < mat[i].size(); j++)
+		{
+			cout << mat[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << "------------------------------------------------" << endl;
+}
+
+
+void PrintThings(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros)
+{
+	cout << "Cost : " << endl;
+	PrintMatrix(Cost);
+	cout << "StarZeros : " << endl;
+	PrintMatrixB(StarZeros);
+	cout << "PrimeZeros : " << endl;
+	PrintMatrixB(PrimeZeros);
+	cout << "CoveredRow : { ";
+	for (int index : CoveredRow)
+	{
+		cout << index << " ";
+	}
+	cout << " }" << endl;
+	cout << "CoveredCol : { ";
+	for (int index : CoveredCol)
+	{
+		cout << index << " ";
+	}
+	cout << " }" << endl;
+}
+
 int GetMaxCost_Matrix(const vector<vector<int>> Cost)
 {
 	vector<int> maxCost_inRow;
@@ -27,6 +75,197 @@ int GetMaxCost_Matrix(const vector<vector<int>> Cost)
 		maxCost_inRow.push_back(*max_element(Row.begin(), Row.end()));
 	}
 	return *max_element(maxCost_inRow.begin(), maxCost_inRow.end());
+}
+
+void step3(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros)
+{
+	//step 3
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			if (StarZeros[i][j] == true)
+			{
+				CoveredCol.push_back(j);
+			}
+		}
+	}
+	if (CoveredCol.size() == MatrixSize)
+	{
+		//종료
+		return;
+	}
+	else
+	{
+		cout << "@@@@ 3 @@@@" << endl;
+		PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+		step4(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+	}
+}
+
+void step4(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros)
+{
+	//step 4
+	vector<int> CopyCoveredRow = CoveredRow;
+	vector<int> CopyCoveredCol = CoveredCol;
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		auto iterR = find(CopyCoveredRow.begin(), CopyCoveredRow.end(), i);
+		if (iterR == CopyCoveredRow.end())
+		{
+			for (int j = 0; j < MatrixSize; j++)
+			{
+				auto iterC = find(CopyCoveredCol.begin(), CopyCoveredCol.end(), j);
+				if (iterC == CopyCoveredCol.end())
+				{
+					if (Cost[i][j] == 0)
+					{
+						PrimeZeros[i][j] = true;
+						auto iterStarRow = find(StarZeros[i].begin(), StarZeros[i].end(), true);
+
+						if (iterStarRow == StarZeros[i].end())
+						{
+							cout << "@@@@ 4 @@@@" << endl;
+							PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+							step5(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+						}
+						else
+						{
+							CoveredRow.push_back(i);
+							int removeCol = distance(StarZeros[i].begin(), iterStarRow);
+							auto removeColIter = find(CoveredCol.begin(), CoveredCol.end(), removeCol);
+							CoveredCol.erase(removeColIter);
+
+						}
+					}
+				}
+
+			}
+		}
+	}
+	cout << "@@@@ 4 @@@@" << endl;
+	PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+	step6(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+}
+
+void step5(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros)
+{
+	vector<vector<bool>> CopyStarZeros = StarZeros;
+	vector<vector<bool>> CopyPrimeZeros = PrimeZeros;
+	bool NewZero = false;
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			if (CopyPrimeZeros[i][j] == true)
+			{
+				for (int k = 0; k < MatrixSize; k++)
+				{
+					if (CopyStarZeros[i][k] == true)
+					{
+						break;
+					}
+					if (k + 1 == MatrixSize)
+					{
+						NewZero = true;
+						StarZeros[i][j] = true;
+					}
+				}
+			}
+			if (NewZero == true)
+			{
+				for (int k = 0; k < MatrixSize; k++)
+				{
+					if (CopyStarZeros[k][j] == true)
+					{
+						StarZeros[k][j] == false;
+						for (int l = 0; l < MatrixSize; l++)
+						{
+							if (CopyPrimeZeros[k][l] == true)
+							{
+								StarZeros[k][l] = true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	if (NewZero == false)
+	{
+		cout << "@@@@ 5 @@@@" << endl;
+		PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+		step6(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+	}
+	else
+	{
+		CoveredCol.clear();
+		CoveredRow.clear();
+		for (int i = 0; i < MatrixSize; i++)
+		{
+			PrimeZeros[i].clear();
+			PrimeZeros[i].assign(MatrixSize, false);
+		}
+		cout << "@@@@ 5 @@@@" << endl;
+		PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+		step3(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+	}
+}
+
+void step6(vector<vector<int>> Cost, int MatrixSize, vector<int> CoveredRow, vector<int> CoveredCol, vector<vector<bool>> StarZeros,
+	vector<vector<bool>> PrimeZeros)
+{
+	//6단계
+	bool first = true;
+	int minCost;
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		auto iterR = find(CoveredRow.begin(), CoveredRow.end(), i);
+		if (iterR == CoveredRow.end())
+		{
+			for (int j = 0; j < MatrixSize; j++)
+			{
+				auto iterC = find(CoveredCol.begin(), CoveredCol.end(), j);
+				if (iterC == CoveredCol.end())
+				{
+					if (first == true)
+					{
+						first = false;
+						minCost = Cost[i][j];
+					}
+					else if (minCost > Cost[i][j])
+					{
+						minCost = Cost[i][j];
+					}
+				}
+			}
+		}
+	}
+	for (int coveredRow : CoveredRow)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			Cost[coveredRow][j] += minCost;
+		}
+	}
+	for (int i = 0; i < MatrixSize; i++)
+	{
+		for (int j = 0; j < MatrixSize; j++)
+		{
+			auto iterC = find(CoveredCol.begin(), CoveredCol.end(), j);
+			if (iterC == CoveredCol.end())
+			{
+				Cost[i][j] -= minCost;
+			}
+		}
+
+	}
+	cout << "@@@@ 6 @@@@" << endl;
+	PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+	step4(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
 }
 
 void HungarianAlgo(vector<vector<int>> Cost, const int N, const int M, const int Mode, int & TotalCost, vector<int> AssignmentIndex)
@@ -91,87 +330,31 @@ void HungarianAlgo(vector<vector<int>> Cost, const int N, const int M, const int
 	//step 2
 	for (int i = 0; i < MatrixSize; i++)
 	{
+		int check = 0;
 		for (int j = 0; j < MatrixSize; j++)
 		{
 			if (Cost[i][j] == 0)
 			{
 				StarZeros[i][j] = true;
+				check = 1;
+				break;
 			}
 		}
+		if (check)
+		{
+			break;
+		}
 	}
-	//step 3
+	PrintThings(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
+
+	step3(Cost, MatrixSize, CoveredRow, CoveredCol, StarZeros, PrimeZeros);
 	for (int i = 0; i < MatrixSize; i++)
 	{
 		for (int j = 0; j < MatrixSize; j++)
 		{
 			if (StarZeros[i][j] == true)
 			{
-				CoveredCol.push_back(j);
-			}
-		}
-	}
-	if (CoveredCol.size() == MatrixSize)
-	{
-		//종료
-	}
-
-	//step 4
-	for (int i = 0; i < MatrixSize; i++)
-	{
-		auto iterR = find(CoveredRow.begin(), CoveredRow.end(), i);
-		if (iterR != CoveredRow.end())
-		{
-			for (int j = 0; j < MatrixSize; j++)
-			{
-				auto iterC = find(CoveredCol.begin(), CoveredCol.end(), j);
-				if (iterC != CoveredCol.end())
-				{
-					if (Cost[i][j] == 0)
-					{
-						PrimeZeros[i][j] = true;
-						auto iterStarRow = find(StarZeros[i].begin(), StarZeros[i].end(), true);
-
-						if (iterStarRow == StarZeros[i].end())
-						{
-							//5단계로 간다
-						}
-						else
-						{
-							CoveredRow.push_back(i);
-							int removeCol = distance(StarZeros[i].begin(), iterStarRow);
-							auto removeColIter = find(CoveredCol.begin(), CoveredCol.end(), removeCol);
-							CoveredCol.erase(removeColIter);
-							
-						}
-					}
-				}
-
-			}
-		}
-		//6단계로 감
-	}
-
-	//step 5
-	vector<vector<bool>> NewZeros(MatrixSize, vector<bool>(MatrixSize, false));
-	for (int i = 0; i < MatrixSize; i++)
-	{
-		for (int j = 0; j < MatrixSize; j++)
-		{
-			if (PrimeZeros[i][j] == true)
-			{
-				for (int k = 0; k < MatrixSize; k++)
-				{
-					if (StarZeros[k][j] == true)
-					{
-						break;
-					}
-					if (k + 1 == MatrixSize)
-					{
-						NewZeros[i][j] = true;
-
-					}
-				}
-
+				AssignmentIndex.push_back(j);
 			}
 		}
 	}
@@ -233,6 +416,12 @@ void main()
 
 	Mode = 0;
 	HungarianAlgo(OriginMatrix, N, M, Mode, TotalCost, AssignmentIndex);
+	cout << "AssignmentIndex : { ";
+	for (int index : AssignmentIndex)
+	{
+		cout << index << " ";
+	}
+	cout << " }" << endl;
 
 
 
