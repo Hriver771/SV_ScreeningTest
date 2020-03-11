7206f930d75eb91b7ff024e2cb24dcb41e5f3bcd
HaMinJi <h.river771@gmail.com>
2020-03-12 02:36:20 +0900

    clean code

--- test2.cpp
+++ test2.cpp
@@ -84,22 +84,23 @@ void Divide_N_Conquer(const vector<int> OriginList, MaxSum& Root)
 			Root.left = Left.left;
 			Root.right = Right.right;
 			Root.sum = Left.sum + Right.sum;
-			cout << "result1.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+			//cout << "result1.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 			return;
 		}
 		else if (Left.sum >= Right.sum)
 		{
 			Root = Left;
-			cout << "result1.2 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+			//cout << "result1.2 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 			return;
 		}
 		else
 		{
 			Root = Right;
-			cout << "result1.3 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+			//cout << "result1.3 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 			return;
 		}
 	}
+	//case2
 	else
 	{
 		int tmpsum = Left.sum;
@@ -122,265 +123,34 @@ void Divide_N_Conquer(const vector<int> OriginList, MaxSum& Root)
 				Right.sum = tmpsum;
 			}
 		}
-		if (Left.right == Right.right)
+		if (Left.right == Right.right || Left.left == Right.left)
 		{
 			if (Left.sum >= Right.sum)
 			{
 				Root = Left;
-				cout << "result2.0(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+				//cout << "result2.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 				return;
 			}
 			else
 			{
-				cout << "이경우 없다 생각함" << endl;
 				Root = Right;
-				cout << "result2.0(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+				//cout << "result2.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 				return;
 			}
-		}
-		else if (Left.left == Right.left)
-		{
-			if (Left.sum <= Right.sum)
-			{
-				Root = Right;
-				cout << "result2.0(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-				return;
-			}
-			else
-			{
-				Root = Left;
-				cout << "이경우 없다 생각함" << endl;
-				cout << "result2.0(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-				return;
-			}
-
 		}
 		else if (Left.sum >= Right.sum)
 				{
 					Root = Left;
-					//cout << "case2.left.3" << endl;
-					cout << "result2.2(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+					//cout << "result2.3 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 					return;
 				}
 		else
 		{
 			Root = Right;
-			//cout << "case2.left.4" << endl;
-			cout << "result2.3(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
+			//cout << "result2.4 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
 			return;
 		}
-
-
-
-
 	}
-	////case2(left)
-	//else if (Left.right == pivot)
-	//{
-	//	int tmpsum = Left.sum;
-	//	for (int i = Left.right + 1; i <= Root.right; i++)
-	//	{
-	//		tmpsum += OriginList[i - 1];
-	//		if (tmpsum > Left.sum)
-	//		{
-	//			Left.right = i;
-	//			Left.sum = tmpsum;
-	//		}
-	//	}
-	//	if (Left.right == Right.right)
-	//	{
-	//		if (Left.sum >= Right.sum)
-	//		{
-	//			Root = Left;
-	//			cout << "result2.0(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//			return;
-	//		}
-	//		else
-	//		{
-	//			Root = Right;
-	//			cout << "result2.0(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//			return;
-	//		}
-	//	}
-	//	else if (Left.right >= Right.left - 1)
-	//	{
-	//		Root = Left;
-	//		cout << "result2.1(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	//if (Left.right >= Right.left)
-	//	//{
-	//	//	Root.left = Left.left;
-	//	//	Root.right = Right.right;
-	//	//	Root.sum = 0;
-	//	//	for (int i = Root.left; i <= Root.right; i++)
-	//	//	{
-	//	//		Root.sum += OriginList[i - 1];
-	//	//	}
-	//	//	cout << "case2.left.1" << endl;
-	//	//	//cout << "result2.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//	//	return;
-	//	//}
-	//	//else if (Left.right + 1 == Right.left)
-	//	//{
-	//	//	if (Left.sum < 0)
-	//	//	{
-	//	//		Root = Right;
-	//	//		cout << "case2.left.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//	else if (Right.sum < 0)
-	//	//	{
-	//	//		Root = Left;
-	//	//		cout << "case2.left.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//	else
-	//	//	{
-	//	//		Root.left = Left.left;
-	//	//		Root.right = Right.right;
-	//	//		Root.sum = Left.sum + Right.sum;
-	//	//		cout << "case2.left.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//}
-	//	else if (Left.sum >= Right.sum)
-	//	{
-	//		Root = Left;
-	//		//cout << "case2.left.3" << endl;
-	//		cout << "result2.2(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	else
-	//	{
-	//		Root = Right;
-	//		//cout << "case2.left.4" << endl;
-	//		cout << "result2.3(left) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//}
-	////case2(right)
-	//else if (Right.left == pivot + 1)
-	//{
-	//	int tmpsum = Right.sum;
-	//	for (int i = Right.left - 1; i >= Root.left; i--)
-	//	{
-	//		tmpsum += OriginList[i - 1];
-	//		if (tmpsum > Right.sum)
-	//		{
-	//			Right.left = i;
-	//			Right.sum = tmpsum;
-	//		}
-	//	}
-	//	if (Left.left == Right.left)
-	//	{
-	//		if (Left.sum >= Right.sum)
-	//		{
-	//			Root = Left;
-	//			cout << "result2.0(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//			return;
-	//		}
-	//		else
-	//		{
-	//			Root = Right;
-	//			cout << "result2.0(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//			return;
-	//		}
-	//	}
-	//	else if (Left.right >= Right.left - 1)
-	//	{
-	//		Root = Right;
-	//		cout << "result2.1(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	//{
-	//	//	Root.left = Left.left;
-	//	//	Root.right = Right.right;
-	//	//	Root.sum = 0;
-	//	//	for (int i = Root.left; i <= Root.right; i++)
-	//	//	{
-	//	//		Root.sum += OriginList[i - 1];
-	//	//	}
-	//	//	cout << "case2.right.1" << endl;
-	//	//	//cout << "result2.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//	//	return;
-	//	//}
-	//	//else if (Left.right + 1 == Right.left)
-	//	//{
-	//	//	if (Left.sum < 0)
-	//	//	{
-	//	//		Root = Right;
-	//	//		cout << "case2.right.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//	else if (Right.sum < 0)
-	//	//	{
-	//	//		Root = Left;
-	//	//		cout << "case2.right.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//	else
-	//	//	{
-	//	//		Root.left = Left.left;
-	//	//		Root.right = Right.right;
-	//	//		Root.sum = Left.sum + Right.sum;
-	//	//		cout << "case2.right.2" << endl;
-	//	//		return;
-	//	//	}
-	//	//}
-	//	else if (Left.sum >= Right.sum)
-	//	{
-	//		Root = Left;
-	//		//cout << "case2.right.3" << endl;
-	//		cout << "result2.2(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	else
-	//	{
-	//		Root = Right;
-	//		//cout << "case2.right.4" << endl;
-	//		cout << "result2.3(right) : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//}
-	////case3
-	//else
-	//{
-	//	if (Left.sum == Right.sum)
-	//	{
-	//		cout << "@@@@ case3 same @@@@" << endl;
-	//	}
-	//	int tmpsum = Left.sum;
-	//	for (int i = Left.right + 1; i < Right.right; i++)
-	//	{
-	//		tmpsum += OriginList[i - 1];
-	//	}
-	//	tmpsum += Right.sum;
-	//	if (tmpsum > Left.sum && tmpsum > Right.sum)
-	//	{
-	//		Root.left = Left.left;
-	//		Root.right = Right.right;
-	//		Root.sum = tmpsum;
-	//		cout << "result3.0 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	else if (Left.sum >= Right.sum)
-	//	{
-	//		Root = Left;
-	//		//cout << "case3.1" << endl;
-	//		cout << "result3.1 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-	//	else
-	//	{
-	//		Root = Right;
-	//		//cout << "case3.2" << endl;
-	//		cout << "result3.2 : " << Root.left << "(left) " << Root.right << "(right) " << Root.sum << "(sum)" << pivot << "(pivot)" << endl;
-	//		return;
-	//	}
-
-	//}
-
 }
 
 int main()
@@ -389,7 +159,9 @@ int main()
 	vector<int> OriginList;
 	MaxSum ResultMaxSumDC;
 	MaxSum ResultMaxSumBF;
+	int n;
 
+	//사용자에게 입력받을 때
 	//cout << "배열을 입력하세요 : ";
 	//getline(cin, strInput);
 
@@ -410,7 +182,9 @@ int main()
 	//OriginList.push_back(atoi(strNum.c_str()));
 
 	srand(static_cast<unsigned int>(time(NULL)));
-	for (int count = 1; count <= 100; ++count)
+
+	n = 1000;
+	for (int count = 1; count <= n; ++count)
 	{
 		int n = rand() % 300 - 150;
 		OriginList.push_back(n);
